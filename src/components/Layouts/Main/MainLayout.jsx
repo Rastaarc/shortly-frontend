f594db75d3ccc83bb97f7b9baa1675ac39e9f5eb
RastaXarm <xarmzon@gmail.com>
2021-06-12 16:58:27 +0100

    update frontend June 12, 2021-2

--- src/components/Layouts/Main/MainLayout.jsx
+++ src/components/Layouts/Main/MainLayout.jsx
@@ -9,22 +9,24 @@ export default function MainLayout({children}) {
     return (
         <>
             <Layout>
-                <Header className="bgMain">
+                <Header style={{padding: 0}} className="bgMain">
                     <div className="header">
                         <div className="logo">
                             <img src="images/logo.png" alt="" />
                         </div>
-                        <div className="rightSide">
+                        <div>
+                            <span className="signInButton">
                             <Button type="primary" shape="round">Sign In</Button>
+                            </span>
                             <Button type="ghost" shape="round">Sign Up</Button>
                         </div>
                     </div>
                 </Header>
-                <Layout>
+                <Layout className="bgMain">
                     {children}
                 </Layout>
                 <Layout>
-                    <Footer>
+                    <Footer className="bgFooter">
                         <div className="footer">
                             &copy; {new Date().getFullYear()}, {APP_NAME}.
                             All right reserved. Made with love by { DEVELOPED_BY }
--- src/pages/General/Landing/LandingPage.jsx
+++ src/pages/General/Landing/LandingPage.jsx
@@ -1,7 +1,8 @@
-import { Col, Row, Typography } from 'antd'
+import { Button, Col, Row, Typography } from 'antd'
 import React from 'react'
 import './LandingPage.less'
 import MainLayout from '../../../components/Layouts/Main/MainLayout'
+import { DEVELOPED_BY } from '../../../utilities/constants'
 
 
 const {Title }  = Typography
@@ -9,17 +10,42 @@ export default function LandingPage() {
     return (
         <>
           <MainLayout>
-              <div className="container">
-                <Row>
-                  <Col xs={{span:24, order: 2}} md={{span:7, order: 1}} >
-                    <Title type={1} className="landingHeading">
+              <div className="container landingHeading">
+                <Row gutter={{md: 6}}>
+                  <Col xs={{span:24, order: 2}} md={{span:9, order: 1}} >
+                    <Title type={1} className="landingHeadingText">
                       When <span style={{color: '#f16059'}}>Shortly</span> the better
                     </Title>
+                    <Title level={4}>
+                      A simple, flexible & powerful URL shortener
+                      tools created by {DEVELOPED_BY} to help
+                      you protect your brand identity.
+                    </Title>
                   </Col>
-                  <Col xs={{span:24, order: 1}} md={{span:17, order: 2}}>
+                  <Col xs={{span:24, order: 1}} md={{span:15, order: 2}}>
                     <div className="displayImg">
-                      <img src="images/_main_img.png" alt="" />
+                      image
+                      {/* <img src="images/_main_img.png" alt="" /> */}
                     </div>
+                  </Col>
+                </Row>
+                <Row style={{margin: "15px 0"}} justify="center">
+                  <Col md={{span: 20}}>
+                    <input type="text"/>
+                  </Col>
+                  <Col md={{span: 4}}>
+                    <Button type="default">Shorten It</Button>
+                  </Col>
+                </Row>
+                <Row gutter="16">
+                  <Col >
+                    Col 1
+                  </Col>
+                  <Col>
+                    Col 2
+                  </Col>
+                  <Col>
+                    Col 3
                   </Col>
                 </Row>
               </div>
