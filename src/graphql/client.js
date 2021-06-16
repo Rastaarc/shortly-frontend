4159487df72bf1bdaf3babfc4af6c5a2391c1571
RastaXarm <xarmzon@gmail.com>
2021-06-16 19:34:10 +0100

    added dynamic endpoint for apollo

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -6,7 +6,7 @@ const client = new ApolloClient({
     uri: process.env.ENDPOINT || 'http://127.0.0.1:5000/graphql-api/',
     cache: new InMemoryCache(),
 
-    name: `${APP_NAME}'s frontend`
+    name: `${APP_NAME}'s frontend app`
 })
 
 export default client
