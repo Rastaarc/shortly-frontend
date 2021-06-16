9d5af477f69f946e0f144f3d2a2f4066178e0637
RastaXarm <xarmzon@gmail.com>
2021-06-16 19:51:11 +0100

    added dynamic endpoint for apollo currently2

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -3,7 +3,7 @@ import {  ApolloClient, InMemoryCache } from '@apollo/client'
 import { APP_NAME } from '../utilities/constants'
 
 const client = new ApolloClient({
-    uri: process.env.REACT_APP_ENDPOINT || 'http://127.0.0.1:5000/graphql-api/',
+    uri: process.env.REACT_APP_API_ENDPOINT || 'http://127.0.0.1:5000/graphql-api/',
     cache: new InMemoryCache(),
 
     name: `${APP_NAME}'s frontend app`
